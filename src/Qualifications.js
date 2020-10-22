d4755d5a3a6d78fb4bede3d5b0561280fb250025
Syed Mehdi <mehdisyed3@gmail.com>
2020-10-22 09:30:27 -0400

    placed contact form into a variable , Removed hover effect

--- src/Qualifications.js
+++ src/Qualifications.js
@@ -3,7 +3,7 @@ import { Card } from 'react-bootstrap'
 
 function Qualifications({ item }) {
   return (
-    <Card className='h-100  shadow-sm bg-white rounded hoverable' >
+    <Card className='h-100  shadow-sm bg-white rounded' >
       <Card.Img className=" mx-auto d-block m-3" max-width="60" height="100" variant="top" src={item.src} />
       <Card.Body className='mx-auto'>
           <Card.Title className='p-1 m-1 text-center align-baseline' >
--- src/Service.js
+++ src/Service.js
@@ -3,7 +3,7 @@ import { Card } from 'react-bootstrap'
 
 function Service({ item }) {
   return (
-    <Card className='h-100  shadow-sm bg-white rounded hoverable' >
+    <Card className='h-100  shadow-sm bg-white rounded' >
       <Card.Img className='' variant="top " src="" />
       <Card.Body className='mx-auto'>
           <Card.Title className='p-1 m-1 text-center' >
