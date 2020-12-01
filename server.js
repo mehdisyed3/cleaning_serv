75a8a3ce0c24e8ac217e61c666020d29d00b2d16
Zain Syed <zeddotes@gmail.com>
2020-12-01 13:21:53 -0500

    - added server code

---
+++ server.js
@@ -0,0 +1,31 @@
+// import path from 'path';
+// import fs from 'fs';
+// import React from 'react';
+const express = require('express')
+// import ReactDOMServer from 'react-dom/server';
+
+// import App from '../src/App';
+
+const app = express();
+
+// app.get('/', (req, res) => {
+//   const app = ReactDOMServer.renderToString(<App />);
+
+//   const indexFile = path.resolve('./build/index.html');
+//   fs.readFile(indexFile, 'utf8', (err, data) => {
+//     if (err) {
+//       console.error('Something went wrong:', err);
+//       return res.status(500).send('Oops, better luck next time!');
+//     }
+
+//     return res.send(
+//       data.replace('<div id="root"></div>', `<div id="root">${app}</div>`)
+//     );
+//   });
+// });
+
+// app.use(express.static('./build'));
+
+app.listen(() => {
+  console.log(`Server is running.`);
+});
