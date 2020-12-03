efed592df6d5604d8813119d25b0fb3688bd955c
Zain Syed <zeddotes@gmail.com>
2020-12-02 21:00:02 -0500

    - cleaned up the contact form

--- src/ContactForm.js
+++ src/ContactForm.js
@@ -1,109 +1,156 @@
-import React, { useState } from 'react'
-import { Form, Button } from 'react-bootstrap'
+import React, { useCallback, useReducer } from 'react'
+import { Form, Button, Alert } from 'react-bootstrap'
 import axios from 'axios'
 
+const R = require('ramda')
 
+const reducer = (formState, action) => {
+  switch (action.type) {
+    case ('formValChange'): {
+      const formStateCopy = R.clone(formState)
+      const { field, val } = action.payload
+      formStateCopy.data[field] = val
+
+      return formStateCopy
+    }
+    case ('formSubmitting'): {
+      console.log("DSDSS")
+      const formStateCopy = R.clone(formState)
+      formStateCopy.submitting = true
+      formStateCopy.submittingErr = null
+      formStateCopy.submittingSuccess = null
+      return formStateCopy
+    }
+    case ('formSubmittingErr'): {
+      const formStateCopy = R.clone(formState)
+      formStateCopy.submitting = false
+      formStateCopy.submittingErr = (<><p className="m-0">Oops, something seems to be broken on our side.</p><p className="m-0">Please try again later or give us a call at { AMPM_PHONE } if it's urgent.</p></>)
+      formStateCopy.submittingSuccess = null
+      return formStateCopy
+    }
+    case ('formSubmittingSuccess'): {
+      const formStateCopy = R.clone(formState)
+      formStateCopy.submitting = false
+      formStateCopy.submittingErr = null
+      formStateCopy.submittingSuccess = (<><p className="m-0">Thanks for reaching out.</p><p className="m-0">Someone from our team will be reaching out to you shortly.</p></>)
+      return formStateCopy
+    }
+    default: {
+      return formState
+    }
+  }
+}
+
+const initialState = {
+  data: {
+    name: 'fewwe',
+    email: 'fewfwe@fewnjfwe.com',
+    phone: '4163000264',
+    msg: 'hello bye'
+  },
+  submittingErr: null,
+  submittingSuccess: null,
+  submitting: false
+}
+
+const AMPM_PHONE = '416-206-3282'
 
 function ContactForm() {
-  const [name, setName] = useState("")
-  const [email, setEmail] = useState("")
-  const [phone, setPhone] = useState("")
-  const [message, setMessage] = useState("")
-  const [didSubmit, setDidSubmit] = useState(false)
-  const [errSubmit, setErrSubmit] = useState(false)
+  const [formState, dispatch] = useReducer(reducer, initialState)
 
-
-  const handleChange = (e) => {
-
-    if (e.target.name === 'name') setName(e.target.value)
-    if (e.target.name === 'email') setEmail(e.target.value)
-    if (e.target.name === 'phone') setPhone(e.target.value)
-    if (e.target.name === 'message') setMessage(e.target.value)
+  const handleChangeForField = (fieldName) => {
+    return function (e) {
+      dispatch({
+        type: 'formValChange',
+        payload: {
+          field: fieldName,
+          val: e.target.value
+        }
+      })
+    }
   }
 
+  const onNameChange = useCallback(handleChangeForField('name'), [])
+  const onEmailChange = useCallback(handleChangeForField('email'), [])
+  const onPhoneChange = useCallback(handleChangeForField('phone'), [])
+  const onMsgChange = useCallback(handleChangeForField('msg'), [])
 
   const handleSubmit = (e) => {
     e.preventDefault()
 
-    const info = {
-      name,
-      email,
-      phone,
-      message
-    }
+    console.log("call disp")
 
-    axios.post('url', info)
+    dispatch({
+      type: 'formSubmitting'
+    })
+
+    // const info = {
+    //   name,
+    //   email,
+    //   phone,
+    //   message
+    // }
+
+    axios.post('/contact', formState.data)
       .then(res => {
-
         console.log(res)
-        setDidSubmit(true)
-
+        dispatch({
+          type: 'formSubmittingSuccess'
+        })
       })
       .catch(err => {
 
         console.log(err)
-        setErrSubmit(true)
+        dispatch({
+          type: 'formSubmittingSuccess'
+        })
       })
 
-    setName("")
-    setEmail("")
-    setMessage("")
-    setPhone("")
+    // setName("")
+    // setEmail("")
+    // setMessage("")
+    // setPhone("")
 
   }
 
-
-  var cForm = (<Form onSubmit={handleSubmit}>
-    <div className='row'>
-      <div className="col-md col-lg text-left">
-        <div className="form-group">
-          <Form.Label htmlFor="name">Name</Form.Label>
-          <Form.Control size="lg" type="text" name="name" value={name} placeholder='Name' onChange={handleChange} />
-        </div>
-      </div>
-      <div className="col-md col-lg text-left">
-        <div className="form-group"> <Form.Label htmlFor="email">Email address</Form.Label>
-          <Form.Control size="lg" name='email' type="email" value={email} placeholder="someone@somwhere.com" onChange={handleChange} />
-        </div>
-      </div>
-      <div className="col-md col-lg text-left">
-        <div className="form-group">
-          <Form.Label htmlFor='phone'>Phone Number </Form.Label>
-          <Form.Control size="lg" type="tel" value={phone} name='phone' pattern="[0-9]{10}" placeholder="Your 10 digit phone number" onChange={handleChange} />
-        </div>
-      </div>
-    </div>
-    <div className="text-left">
-      <div className="form-group">
-        <Form.Label htmlFor="message">Message</Form.Label>
-        <Form.Control size="lg" rows='4' as="textarea" value={message} name='message' onChange={handleChange} placeholder='Type in your message' />
-      </div>
-    </div>
-    <div className="mt-3 text-left">
-      <Button disabled={email === "" && phone === "" ? true : false} size="lg" type="submit" className="btn btn-primary">Send</Button>
-    </div>
-  </Form>)
-
   return (
     <>
-      <p className='lead'>You can call us at 416-206-3282 or use the form below to get in touch with us or receive a FREE Quote.</p>
-      <p className="mb-4"><strong>Please be advised that your information is never shared or sold.</strong></p>
-      {didSubmit ?
-        <p className='text-success text-center'>
-          Thank You. Your inquiry has been submitted.
-        </p>
-        :
-        errSubmit ?
+      <p className='lead'>You can call us at { AMPM_PHONE } or use the form below to get in touch with us or receive a FREE Quote.</p>
+      <p className="mb-4"><strong>Please be advised that your information is <u>never shared or sold</u>.</strong></p>
 
-          <div>
-            <p className='text-danger text-center'>
-              Oops. Something went wrong. Please Try again later.
-          </p>
-
-            {cForm}
-          </div>
-          :
-          cForm
+      { formState.submittingErr && <Alert variant="warning">{ formState.submittingErr }</Alert>}
+      {
+        formState.submittingSuccess ? <Alert variant="success">{ formState.submittingSuccess }</Alert> :
+          <Form onSubmit={handleSubmit}>
+            <div className='row'>
+              <div className="col-md col-lg text-left">
+                <div className="form-group">
+                  <Form.Label htmlFor="name"><code className="lead">*</code>Name</Form.Label>
+                  <Form.Control size="lg" required type="text" name="name" value={formState.data.name} onChange={onNameChange} />
+                </div>
+              </div>
+              <div className="col-md col-lg text-left">
+                <div className="form-group"> <Form.Label htmlFor="email"><code className="lead">*</code>Email address</Form.Label>
+                  <Form.Control size="lg" name='email' required type="email" value={formState.data.email} placeholder="john@example.com" onChange={onEmailChange} />
+                </div>
+              </div>
+              <div className="col-md col-lg text-left">
+                <div className="form-group">
+                  <Form.Label htmlFor='phone'><code className="lead"></code>Phone</Form.Label>
+                  <Form.Control size="lg" type="tel" maxLength={10} name='phone' value={formState.data.phone} pattern="[0-9]{10}" placeholder="Only numbers" onChange={onPhoneChange} />
+                </div>
+              </div>
+            </div>
+            <div className="text-left">
+              <div className="form-group">
+                <Form.Label htmlFor="message"><code className="lead">*</code>Message</Form.Label>
+                <Form.Control size="lg" rows='4' required as="textarea" name='message' value={formState.data.msg} onChange={onMsgChange} placeholder='Describe how we may serve you' />
+              </div>
+            </div>
+            <div className="mt-3 text-left">
+              <Button size="lg" disabled={formState.submitting} type="submit" className="btn btn-primary">{formState.submitting ? 'Sending...' : 'Send'}</Button>
+            </div>
+          </Form>
       }
     </>
   )
